6f1620842da93ff14974d8eb9da428095a19538c
ANAHITA SINGHANIA <85817414+singhania0601@users.noreply.github.com>
2022-02-15 00:59:20 +0530

    Time: 0 ms (100.00%), Space: 6 MB (39.62%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
-    int fib(int n) {
-        if (n == 0)
-            return 0;
-        if( n == 1 )
-            return 1;
-        return fib(n-1)+fib(n-2);
+    int fib(int N) {
+      vector<int> memo(N+1,0);
+        if(N == 0) return 0;
+        if(N == 1) return 1;
+        memo[0] = 0;
+        memo[1] = 1;
+        for(int i = 2; i < N+1; i++){
+            memo[i] = memo[i-1]+memo[i-2];
+        }
+        return memo[N];
         
     }
 };
