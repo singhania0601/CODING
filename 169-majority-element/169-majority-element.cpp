ad5b87ec2d57c39ad78226cd704137aa1ad1c9d0
ANAHITA SINGHANIA <85817414+singhania0601@users.noreply.github.com>
2022-07-09 19:00:31 +0530

    Time: 22 ms (82.18%), Space: 19.6 MB (58.27%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        
-        unordered_map<int,int> hmap; 
-        for(int i=0;i<nums.size();i++)
-        {
-            hmap[nums[i]]++;
-        }
-        
-        int cMax = 0;
-        int max = 0;
-        
-        for(auto c: hmap)
-        {
-            if(c.second > cMax)
-            {
-                cMax = c.second;
-                max = c.first;
-            }
-        }
-        
-        return max;
-        
+    int majorityElement(vector<int>& a) {
+         int majorityCount = ((int) a.size()) / 2;
+    unordered_map <int , int> frequency;
+    for(int &i : a)
+    {
+        if(++frequency[i] > majorityCount)
+            return i;
+    }
+    return -1;
     }
 };
